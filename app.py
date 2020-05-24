03b6fab878fa78ca5df03dcf0d2ec84c898f1b28
Qylwok <36070776+Qylwok@users.noreply.github.com>
2020-05-24 11:33:05 +0200

    deploying the dashboard

--- app.py
+++ app.py
@@ -826,6 +826,6 @@ def update_figure(input_value):
 
 # Run app
 if __name__ == "__main__":
-    # app.run_server(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run_server(debug=True)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(debug=True, host='0.0.0.0', port=port)
