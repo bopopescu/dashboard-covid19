8d8ffef639e0f95993c42f1b56b8e98a93dfdf2a
Qylwok <36070776+Qylwok@users.noreply.github.com>
2020-05-24 11:50:24 +0200

    deploying the dashboard

--- app.py
+++ app.py
@@ -829,4 +829,4 @@ if __name__ == "__main__":
     # app.run_server(debug=True)
     # port = int(os.environ.get("PORT", 5000))
     # app.run(debug=True, host='0.0.0.0', port=port)
-    app.run(debug=True)
+    app.server.run(debug=True)
